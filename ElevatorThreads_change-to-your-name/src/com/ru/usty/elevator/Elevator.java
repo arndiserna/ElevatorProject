9413e619d4c08ced74b58cf116d0843fcee3e301
Omarv <omarkval@gmail.com>
2016-02-26 14:20:08 +0000

    testcase 2 næstu komið held þessi lausn virka like fyrir 3

--- ElevatorThreads_change-to-your-name/src/com/ru/usty/elevator/Elevator.java
+++ ElevatorThreads_change-to-your-name/src/com/ru/usty/elevator/Elevator.java
@@ -14,62 +14,64 @@ public class Elevator implements Runnable {
 			if(ElevatorScene.elevatorsMayDie) {
 				return;
 			}
-			if(scene.getCurrentFloorForElevator(0) == 0) {
-				try {
-					ElevatorScene.elevatorWaitMutex.acquire();
+			/*try {
+
+			
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}*/
+			int numberOfFloor = scene.getNumberOfFloors();
+			try {
 				
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				int space = scene.checkSpaceInElevator();
-				ElevatorScene.elevatorWaitMutex.release();
-				if(space != 0) {
-					for(int i = 0; i < space; i++) {
+				
+				for(int i = 0; i < numberOfFloor; i++){
+					
+					ElevatorScene.elevatorWaitMutex.acquire();
+					int leaveElevator = scene.leaveThisFloor(scene.getCurrentFloorForElevator(0));
+					ElevatorScene.elevatorWaitMutex.release();
+					for(int x = 0; x < leaveElevator ; x++) {
 						try {
 							Thread.sleep(ElevatorScene.VISUALIZATION_WAIT_TIME);
-							ElevatorScene.in[scene.getCurrentFloorForElevator(0)].release();
-							
+							//ElevatorScene.elevatorWaitMutex.acquire();
+							ElevatorScene.out[scene.getCurrentFloorForElevator(0)].release();
+							//ElevatorScene.elevatorWaitMutex.release();
 						} catch (InterruptedException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
+						
 					}
-					try {
-						
-						
-						for(int i = 0; i < scene.getNumberOfFloors(); i++){
-							ElevatorScene.elevatorWaitMutex.acquire();
-							int leaveElevator = scene.leaveThisFloor(scene.getCurrentFloorForElevator(0));
-							ElevatorScene.elevatorWaitMutex.release();
-							for(int x = 0; x < leaveElevator ; x++) {
-								try {
-									Thread.sleep(ElevatorScene.VISUALIZATION_WAIT_TIME);
-									//ElevatorScene.elevatorWaitMutex.acquire();
-									ElevatorScene.out[scene.getCurrentFloorForElevator(0)].release();
-									//ElevatorScene.elevatorWaitMutex.release();
-								} catch (InterruptedException e) {
-									// TODO Auto-generated catch block
-									e.printStackTrace();
-								}
+					ElevatorScene.elevatorWaitMutex.acquire();
+					int space = scene.checkSpaceInElevator();
+					ElevatorScene.elevatorWaitMutex.release();
+					if(space != 0) {
+						for(int x = 0; x < space; x++) {
+							try {
+								Thread.sleep(ElevatorScene.VISUALIZATION_WAIT_TIME);
+								ElevatorScene.in[scene.getCurrentFloorForElevator(0)].release();
 								
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
 							}
-							Thread.sleep(ElevatorScene.VISUALIZATION_WAIT_TIME);
-							if(scene.getNumberOfFloors() - 1 != scene.getCurrentFloorForElevator(0)) {
-								scene.incrementFloor(0);
-							}
-							System.out.println("up");
-							//System.out.println(scene.getCurrentFloorForElevator(0) + " had");
-			
 						}
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
 					}
+					Thread.sleep(ElevatorScene.VISUALIZATION_WAIT_TIME);
+					if(numberOfFloor - 1 != scene.getCurrentFloorForElevator(0)) {
+						scene.incrementFloor(0);
+					}
+					System.out.println("up");
+					//System.out.println(scene.getCurrentFloorForElevator(0) + " had");
+	
 				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			
 			try {
-				for(int i = 0; i < scene.getNumberOfFloors(); i++) {
+				for(int i = 0; i < numberOfFloor; i++) {
 					//Thread.sleep(ElevatorScene.VISUALIZATION_WAIT_TIME);
 					/*for(int x = 0; x < scene.getNumberOfPeopleInElevator(0); x++) {
 						
