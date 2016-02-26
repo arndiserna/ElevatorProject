a2ac8c5cddedabf52fbbe57c85d4e6543df1099c
Omarv <omarkval@gmail.com>
2016-02-26 15:21:27 +0000

    upp og nidur

--- ElevatorThreads_change-to-your-name/src/com/ru/usty/elevator/Elevator.java
+++ ElevatorThreads_change-to-your-name/src/com/ru/usty/elevator/Elevator.java
@@ -79,6 +79,33 @@ public class Elevator implements Runnable {
 			
 			try {
 				for(int i = 0; i < numberOfFloor; i++) {
+					
+					ElevatorScene.elevatorWaitMutex.acquire();
+					int leaveElevator = scene.leaveThisFloor(scene.getCurrentFloorForElevator(0));
+					if(leaveElevator == 0){
+						ElevatorScene.allOut.release();
+					}
+					ElevatorScene.elevatorWaitMutex.release();
+					for(int x = 0; x < leaveElevator ; x++) {
+						Thread.sleep(ElevatorScene.VISUALIZATION_WAIT_TIME);
+						//ElevatorScene.elevatorWaitMutex.acquire();
+						ElevatorScene.out[scene.getCurrentFloorForElevator(0)].release();
+						//ElevatorScene.elevatorWaitMutex.release();
+					}
+					ElevatorScene.allOut.acquire();
+					ElevatorScene.elevatorWaitMutex.acquire();
+					int space = scene.checkSpaceInElevator();
+					//int waitingAtFloor = scene.getNumberOfPeopleWaitingAtFloor(scene.getCurrentFloorForElevator(0));
+					//System.out.println(waitingAtFloor + " bíða");
+					ElevatorScene.elevatorWaitMutex.release();
+					if(space != 0) { //&& 6 < waitingAtFloor
+						for(int x = 0; x < space; x++) {
+							Thread.sleep(ElevatorScene.VISUALIZATION_WAIT_TIME);
+							ElevatorScene.in[scene.getCurrentFloorForElevator(0)].release();
+						}
+					}
+					
+					
 					Thread.sleep(ElevatorScene.VISUALIZATION_WAIT_TIME);
 					scene.decrementFloor(0);
 					System.out.println("nidur");
