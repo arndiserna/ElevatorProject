8f1f49a390723d5c5071aee423b73aefcf8401bf
Omarv <omarkval@gmail.com>
2016-03-03 00:42:16 +0000

    for ekki mem

--- ElevatorThreads_change-to-your-name/src/com/ru/usty/elevator/Person.java
+++ ElevatorThreads_change-to-your-name/src/com/ru/usty/elevator/Person.java
@@ -15,37 +15,42 @@ public class Person implements Runnable{
 	@Override
 	public void run() {
 		try {
-			//ElevatorScene.elevatorWaitMutex.acquire();
-				//ElevatorScene.personSemaphore.acquire();//Wait
 				
 				scene.incrementNumberOfPeopleWaitingAtFloor(in);
-				//ElevatorScene.personSemaphore.release();
 				ElevatorScene.in[in].acquire();
 				ElevatorScene.insideMutex.acquire();
-				inElevatorId = scene.getElevatorID();
 				
+				ElevatorScene.getIDSemaphore.acquire();
+				inElevatorId = scene.getElevatorID();
+				ElevatorScene.personSemaphore.release();
 				scene.incLeaveThisFloor(out, inElevatorId);
+				//ElevatorScene.personSemaphore.acquire();
 				scene.incrementPeopleInElevator(inElevatorId);
 				scene.decrementNumberOfPeopleWaitingAtFloor(in);
-				System.out.println("in");
+				//System.out.println("in");
 				ElevatorScene.insideMutex.release();
-				//ElevatorScene.elevatorWaitMutex.acquire();
-				ElevatorScene.out[out].acquire();
-				System.out.println("UT");
+				//ElevatorScene.out[out].acquire();
+				ElevatorScene.testout[inElevatorId][out].acquire();
+				ElevatorScene.outsideMutex.acquire();
+				//System.out.println("UT");
 				scene.decLeaveThisFloor(out, inElevatorId);
 				scene.personExitsAtFloor(out);
-				//ElevatorScene.elevatorWaitMutex.release();
 				scene.decrementPeopleInElevator(inElevatorId);
+				ElevatorScene.checkedOutSemaphore.release();
+				//while(scene.leaveThisFloor(out, inElevatorId) != 0 || scene.leaveThisFloor(out, inElevatorId) == 0) {
+				System.out.println(scene.leaveThisFloor(out, inElevatorId) + " eftir " +inElevatorId);
 				if(scene.leaveThisFloor(out, inElevatorId) == 0) {
-					ElevatorScene.allOut.release();
+					System.out.println("lyfta laus "+ inElevatorId);
+					ElevatorScene.allOut[inElevatorId].release();
 				}
-
+				
+				ElevatorScene.outsideMutex.release();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
 		
-		System.out.println("Person thread released");
+		//System.out.println("Person thread released");
 	}
 
 }
